d2ba5294450f1ad14ad1ea9920cfadf562be535c
robbeeen <76037231+robbeeen@users.noreply.github.com>
2024-12-02 18:20:18 +0530

    changes

--- maintonia/settings.py
+++ maintonia/settings.py
@@ -142,10 +142,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
