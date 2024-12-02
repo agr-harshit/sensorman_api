92516ae059a0772e1adbf66240c3b31aaf61d513
robbeeen <76037231+robbeeen@users.noreply.github.com>
2024-12-02 17:52:56 +0530

    changes

--- maintonia/settings.py
+++ maintonia/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
