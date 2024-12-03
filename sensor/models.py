61c2bf0a055d365fd7c4aa46d392130e1592db28
robbeeen <76037231+robbeeen@users.noreply.github.com>
2024-12-03 13:06:30 +0530

    changes

--- sensor/models.py
+++ sensor/models.py
@@ -90,6 +90,3 @@ class SensorReading(models.Model):
     bt_low_cut_v = models.CharField(max_length=255, blank=True, null=True)
     bt_hi_cut_v = models.CharField(max_length=255, blank=True, null=True)
     cpu_temp = models.CharField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return self.record_number
